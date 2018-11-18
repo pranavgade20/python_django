a7f87e8d5d3da58cb1715fda73cfac290547d4ed
Pranav Gade <pranavgade20@gmail.com>
2018-11-18 22:02:19 +0530

    Update views.py
    Remove unused dependencies

--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
 from django.http import HttpResponse
 
 import pymongo
 from pymongo import MongoClient
-import pprint
-import json
-
 client = MongoClient('localhost', 27017)
 db = client.test
 collection = db.documents
